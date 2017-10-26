aef9a4d76cd9a91d4b8805eaa04acac6dac7a0fa
陈云杰 <chenyunjie@qianmi.com>
2017-10-26 20:10:55 +0800

    -

---
+++ frameworks/ArenaShare.framework/Headers/ArenaShare.h
@@ -0,0 +1,18 @@
+//
+//  ArenaShare.h
+//  ArenaShare
+//
+//  Created by 陈云杰 on 2017/10/26.
+//  Copyright © 2017年 江苏斑马软件技术有限公司. All rights reserved.
+//
+
+
+//! Project version number for ArenaShare.
+FOUNDATION_EXPORT double ArenaShareVersionNumber;
+
+//! Project version string for ArenaShare.
+FOUNDATION_EXPORT const unsigned char ArenaShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArenaShare/PublicHeader.h>
+
+
